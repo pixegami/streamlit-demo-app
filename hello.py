0b6ea047f39eaa25dfc9cee4de28f5d93ac3d3dc
Jack (Pixegami) <pixegami@gmail.com>
2024-06-06 20:42:54 +1000

    add data file

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.write("Hello World")
 st.write("## This is a H2 Title!")
@@ -6,3 +8,12 @@ x = st.text_input("Movie", "Star Wars")
 
 if st.button("Click Me"):
     st.write(f"Your favorite movie is `{x}`")
+
+
+data = pd.read_csv("movies.csv")
+st.write(data)
+
+
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
